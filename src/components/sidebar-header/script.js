97cb0d9b13b8265dba63e75d90f584c1c4232875
Paamzzz <pamamanciogoulart@gmail.com>
2025-11-01 13:43:32 -0300

    feat: iniciado o js do sidebar

---
+++ src/components/sidebar-header/script.js
@@ -0,0 +1,8 @@
+const body = document.querySelector("body"),
+     sidebar = body.querySelector(".sidebar"),
+     sidebar_btn = document.getElementById("sidebar_btn");
+     layout_wrapper = 
+
+     sidebar_btn.addEventListener("click", () => {
+          sidebar.classList.toggle("on");
+     });
