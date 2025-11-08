f2714249e209e241cd6425e6c3b71874ee647129
Paamzzz <pamamanciogoulart@gmail.com>
2025-11-08 00:04:25 -0300

    feat: darkmode feito

--- public/assets/js/acessibilidade.js
+++ public/assets/js/acessibilidade.js
@@ -3,23 +3,32 @@
    ======================================== */
 
    let darkmode = localStorage.getItem('darkmode');
-   const darkmode_btn = document.querySelector('.btn_darkmode');
+   const btn_darkmode = document.querySelector('.btn_darkmode');
    const darkmode_icon = btn_darkmode.querySelector('i');
 
    const ligarDarkMode = () => {
-     document.body.classList.add('darkmode');
-     darkmode_icon.classList.replace('bi-moon', 'bi_sun');
+     document.documentElement.classList.add('darkmode');
+     darkmode_icon.classList.replace('bi-moon', 'bi-sun');
      localStorage.setItem('darkmode', 'ligado');
    };
 
    const desligarDarkMode = () => {
-     document.body.classList.remove('darkmode');
-     darkmode_icon.classList.replace('bi-moon', 'bi_sun');
+     document.documentElement.classList.remove('darkmode');
+     darkmode_icon.classList.replace('bi-sun', 'bi-moon');
      localStorage.setItem('darkmode', 'desligado');
    }
 
-   if (darkmode === 'ligado') {
+   if (darkmode === 'ligado') { //ao abrir página, confirmar o estado no localStorage
      ligarDarkMode();
    } else {
      desligarDarkMode();
    }
+
+   btn_darkmode.addEventListener('click', () => {
+     darkmode = localStorage.getItem('darkmode');
+     if (darkmode === 'ligado') {
+           desligarDarkMode();
+     } else {
+          ligarDarkMode();
+     }
+   })
